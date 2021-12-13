dc9592cbd16ff8663a010f583e4f62767d05f73f
Neville <neville.rabakale@gmail.com>
2021-12-13 21:51:11 +0100

    Added, ApplicationDbContext, Model, Services for Db, Db in Appsetting, and Index Page

---
+++ NoteApp/Pages/Notes/Index.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using NoteApp.Model;
+
+namespace NoteApp.Pages.Notes
+{
+    public class IndexModel : PageModel
+    {
+        private readonly NoteApp.Model.ApplicationDbContext _context;
+
+        public IndexModel(NoteApp.Model.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<NoteModel> NoteModel { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            NoteModel = await _context.NoteModel.ToListAsync();
+        }
+    }
+}
