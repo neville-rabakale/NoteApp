8dd5819ee461519b9d0b96a9053bfbc470a79af5
Neville <neville.rabakale@gmail.com>
2021-12-22 21:04:15 +0100

    Implemeneted details page

--- NoteApp/Model/NoteModel.cs
+++ NoteApp/Model/NoteModel.cs
@@ -12,6 +12,6 @@ namespace NoteApp.Model
         public int Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
-        public String Date { get; set; }
+        public string Date { get; set; }
     }
 }
---
+++ NoteApp/Pages/Notes/Details.cshtml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using NoteApp.Model;
+
+namespace NoteApp.Pages.Notes
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly NoteApp.Model.ApplicationDbContext _db;
+
+        public DetailsModel(NoteApp.Model.ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public NoteModel Notes { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Notes = await _db.NoteModel.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (Notes == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
